5165ca1f9fb0f5f034c85130f86ad66b53cea20d
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2024-11-17 11:47:37 -0500

    fixed vma headers not found on macos in /usr/local/include

--- build.sh
+++ build.sh
@@ -12,6 +12,19 @@ release_build="-O2"
 compiler="gcc"
 
 build_type="$debug_build"
+vulkan_sdk=""
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  # default installation directory
+  vulkan_sdk=`echo ${HOME}/VulkanSDK/*/macOS`
+  if [ ! -e "$vulkan_sdk" ]; then
+    if [ -n "$VULKAN_SDK" ]; then
+      vulkan_sdk="$VULKAN_SDK"
+    else  
+      echo "vulkan sdk not found.\n    export VULKAN_SDK=path/to/VulkanSDK" && exit 1
+    fi
+  fi
+fi
 
 [ "$release" == "1" ] && build_type="$release_build"
 [ "$clang" == "1" ] && compiler="clang"
@@ -19,8 +32,7 @@ build_type="$debug_build"
 echo "[$compiler]"
 echo "[$build_type]"
 
-[ "$ext" == "1" ] && g++ -std=c++17 $build_type -I. -c vma.cpp -o vma.o && echo "compiled vma"
 
-$compiler -Wall -Wextra -Wno-unused-function -Wno-int-conversion -Wno-incompatible-pointer-types -Wno-sign-compare -Wno-unused-parameter -std=c99 -D_GNU_SOURCE $build_type -I. -c ./main.c -o main.o && echo "compiled yk"
+[ "$ext" == "1" ] && g++ -std=c++17 $build_type -I. -I$vulkan_sdk/include/vma -c vma.cpp -o vma.o && echo "compiled vma"
 
-g++ main.o vma.o -o yk -lm -lglfw && echo "linked yk and vma"
+g++ main.o vma.o -o yk -lm -lglfw && echo "linked yk and vma"$compiler -Wall -Wextra -Wno-unused-function -Wno-int-conversion -Wno-incompatible-pointer-types -Wno-sign-compare -Wno-unused-parameter -std=c99 -D_GNU_SOURCE $build_type -I. -I$vulkan_sdk/include/vma -c ./main.c -o main.o && echo "compiled yk"
