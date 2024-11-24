061aaa7c67eb7d29bb45930dc8894a032ef38df6
oh-facts <as17793@nyu.edu>
2024-11-23 23:27:28 -0500

    spacing

--- src/base_file.c
+++ src/base_file.c
@@ -4,146 +4,143 @@
 typedef enum FILE_TYPE FILE_TYPE;
 enum FILE_TYPE
 {
-	FILE_TYPE_TEXT,
-	FILE_TYPE_BINARY,
-	FILE_TYPE_COUNT
+ FILE_TYPE_TEXT,
+ FILE_TYPE_BINARY,
+ FILE_TYPE_COUNT
 };
 
 typedef struct FileData FileData;
 struct FileData
 {
-	u8 *bytes;
-	u64 size;
+ u8 *bytes;
+ u64 size;
 };
 
 #if defined(OS_WIN32)
 #define fileOpenImpl(file, filepath, mode) fopen_s(file, filepath, mode)
-#elif defined (OS_LINUX) || defined (OS_APPLE)
+#elif defined(OS_LINUX) || defined(OS_APPLE)
 #define fileOpenImpl(file, filepath, mode) *file = fopen(filepath, mode)
 #endif
 
 function FileData readFile(Arena *arena, Str8 filepath, FILE_TYPE type)
 {
-	FileData out = {0};
-	FILE *file;
-	
-	read_only char *file_type_table[FILE_TYPE_COUNT] = 
-	{
-		"r",
-		"rb"
-	};
-	
+ FileData out = {0};
+ FILE *file;
+
+ read_only char *file_type_table[FILE_TYPE_COUNT] =
+     {
+         "r",
+         "rb"};
+
  ArenaTemp temp = scratch_begin(0, 0);
  Str8 dir = os_getAppDir(temp.arena);
- Str8 abs_path = str8_join(temp.arena, dir, filepath);	
-	
-	fileOpenImpl(&file, abs_path.c, file_type_table[type]);
-	
-	fseek(file, 0, SEEK_END);
-	
-	out.size = ftell(file);
-	//print("%d", len);
-	
-	fseek(file, 0, SEEK_SET);
-	
-	out.bytes = pushArray(arena, u8, out.size);
-	fread(out.bytes, sizeof(u8), out.size, file);
-	
-	fclose(file);
-	
-	return out;
+ Str8 abs_path = str8_join(temp.arena, dir, filepath);
+
+ fileOpenImpl(&file, abs_path.c, file_type_table[type]);
+
+ fseek(file, 0, SEEK_END);
+
+ out.size = ftell(file);
+ // print("%d", len);
+
+ fseek(file, 0, SEEK_SET);
+
+ out.bytes = pushArray(arena, u8, out.size);
+ fread(out.bytes, sizeof(u8), out.size, file);
+
+ fclose(file);
+
+ return out;
 }
 
 function void writeFile(const char *filepath, FILE_TYPE type, void *data, size_t size)
 {
-	FILE *file;
-	
-	read_only char *file_type_table[FILE_TYPE_COUNT] = 
-	{
-		"w",
-		"wb"
-	};
-	
-	fileOpenImpl(&file, filepath, file_type_table[type]);
-	
-	fwrite(data, size, 1, file);
-	
-	fclose(file);
-	
+ FILE *file;
+
+ read_only char *file_type_table[FILE_TYPE_COUNT] =
+     {
+         "w",
+         "wb"};
+
+ fileOpenImpl(&file, filepath, file_type_table[type]);
+
+ fwrite(data, size, 1, file);
+
+ fclose(file);
 }
 
-function b32 copyFile(const char* sourcePath, char* destinationPath)
+function b32 copyFile(const char *sourcePath, char *destinationPath)
 {
-	b32 out = 0;
-	
-	FILE* sourceFile, * destinationFile;
-	char buffer[4096];
-	size_t bytesRead;
-	
-	fileOpenImpl(&sourceFile, sourcePath, "rb");
-	
-	if(sourceFile)
-	{
-		fileOpenImpl(&destinationFile, destinationPath, "wb");
-		
-		if(destinationFile)
-		{
-			while ((bytesRead = fread(buffer, 1, sizeof(buffer), sourceFile)) > 0) 
-			{
-				fwrite(buffer, 1, bytesRead, destinationFile);
-			}
-			
-			fclose(sourceFile);
-			fclose(destinationFile);
-			
-			out = 1;
-		}
-	}
-	
-	return out;
+ b32 out = 0;
+
+ FILE *sourceFile, *destinationFile;
+ char buffer[4096];
+ size_t bytesRead;
+
+ fileOpenImpl(&sourceFile, sourcePath, "rb");
+
+ if (sourceFile)
+ {
+  fileOpenImpl(&destinationFile, destinationPath, "wb");
+
+  if (destinationFile)
+  {
+   while ((bytesRead = fread(buffer, 1, sizeof(buffer), sourceFile)) > 0)
+   {
+    fwrite(buffer, 1, bytesRead, destinationFile);
+   }
+
+   fclose(sourceFile);
+   fclose(destinationFile);
+
+   out = 1;
+  }
+ }
+
+ return out;
 }
 
 function Str8 fileNameFromPath(Arena *arena, Str8 path)
 {
-	char *cur = (char*)&path.c[path.len - 1];
-	u32 count = 0;
-	
-	//NOTE(mizu): pig
-	while(*cur != '/' && *cur != '\\' && *cur != '\0')
-	{
-		cur--;
-		count++;
-	}
-	
-	Str8 file_name_cstr = {0};
-	file_name_cstr.c = pushArray(arena, u8, count + 1);
-	file_name_cstr.len = count + 1;
-	memcpy(file_name_cstr.c, cur + 1, count);
-	file_name_cstr.c[count] = '\0';
-	
-	return file_name_cstr;
+ char *cur = (char *)&path.c[path.len - 1];
+ u32 count = 0;
+
+ // NOTE(mizu): pig
+ while (*cur != '/' && *cur != '\\' && *cur != '\0')
+ {
+  cur--;
+  count++;
+ }
+
+ Str8 file_name_cstr = {0};
+ file_name_cstr.c = pushArray(arena, u8, count + 1);
+ file_name_cstr.len = count + 1;
+ memcpy(file_name_cstr.c, cur + 1, count);
+ file_name_cstr.c[count] = '\0';
+
+ return file_name_cstr;
 }
 
 typedef struct Bitmap Bitmap;
 struct Bitmap
 {
-	void *data;
-	s32 w;
-	s32 h;
-	s32 n;
+ void *data;
+ s32 w;
+ s32 h;
+ s32 n;
 };
 
 function Bitmap bitmap(Str8 path)
 {
-	Bitmap out = {0};
-	
-	stbi_set_flip_vertically_on_load(1);
-	ArenaTemp temp = scratch_begin(0, 0);
-	Str8 dir = os_getAppDir(temp.arena);
-	Str8 abs_path = str8_join(temp.arena, str8_lit("../res/"), path);	
-	abs_path = str8_join(temp.arena, dir, abs_path);	
-	out.data = stbi_load((char*)abs_path.c, &out.w, &out.h, &out.n, 0);
-	
-	scratch_end(&temp);
-	return out;
+ Bitmap out = {0};
+
+ stbi_set_flip_vertically_on_load(1);
+ ArenaTemp temp = scratch_begin(0, 0);
+ Str8 dir = os_getAppDir(temp.arena);
+ Str8 abs_path = str8_join(temp.arena, str8_lit("../res/"), path);
+ abs_path = str8_join(temp.arena, dir, abs_path);
+ out.data = stbi_load((char *)abs_path.c, &out.w, &out.h, &out.n, 0);
+
+ scratch_end(&temp);
+ return out;
 }
